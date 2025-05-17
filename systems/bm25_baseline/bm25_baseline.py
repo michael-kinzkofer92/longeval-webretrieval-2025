5bad0458beff22cfdcadaefd42f607cfc9e35e4f
balexhu <46222324+balexhu@users.noreply.github.com>
2025-05-17 11:38:14 +0200

    Baseline class

---
+++ systems/bm25_baseline/bm25_baseline.py
@@ -0,0 +1,62 @@
+import pandas as pd
+from pyserini.search.lucene import LuceneSearcher
+import os
+
+# BM25 baseline + traditional model as a class
+# based on script/search.py + instructions in Word
+
+class BM25Baseline:
+    """
+    Class implementing BM25 baseline model
+    """
+    run_id = 'bm25-baseline+traditional'
+
+
+    def __init__(self, index_path: str, queries_file_path: str, run_file_path: str):
+        self.index_path = index_path
+        self.queries_file_path = queries_file_path
+        self.run_file_path = run_file_path
+
+    def parse_queries(self) -> pd.DataFrame:
+        # Parse queries.trec manually
+        queries = []
+
+        with open(self.queries_file_path, 'r') as f:
+            qid, query = None, None
+            for line in f:
+                if line.startswith('<num>'):
+                    qid = line.strip()
+                    qid = qid.replace('<num>', '').replace('</num>', '')
+                    qid = qid.replace('Number:', '').strip()
+                if line.startswith('<title>'):
+                    query = line.replace('<title>', '').strip()
+                    queries.append({'qid': qid, 'query': query})
+
+        return pd.DataFrame(queries)
+
+    def run_search(self, k1, b, top_k):
+        # Load searcher
+        searcher = LuceneSearcher(self.index_path)
+
+        queries_df = self.parse_queries()
+
+        # Set BM25 parameters (if not set, they stay at k1 = 0.9, b = 0.4)
+        searcher.set_bm25(k1=k1, b=b)
+
+        # Create runs/ output directory if missing
+        os.makedirs(os.path.dirname(self.run_file_path), exist_ok=True)
+
+        # Write results in TREC format
+        with open(self.run_file_path, 'w') as f_out:
+            for _, row in queries_df.iterrows():
+                qid, query = str(row.qid), row.query
+                hits = searcher.search(query, k=top_k)
+                print(f"Query {qid} → Top hits:", [hit.docid for hit in hits[:10]])
+                for rank, hit in enumerate(hits):
+                    docid = hit.docid
+                    f_out.write(f"{qid} Q0 {docid} {rank+1} {hit.score:.4f} {self.run_id}\n")
+
+        print(f"✅ Test run written to {self.run_file_path}")
+
+
+
---
+++ systems/bm25_baseline/optimize.py
@@ -0,0 +1,44 @@
+import os
+import yaml
+from bm25_baseline import BM25Baseline
+
+# Define script & project path
+script_path = os.path.dirname(os.path.abspath(__file__))
+project_path = os.path.dirname(os.path.dirname(script_path))
+
+# Load configuration
+config_path = os.path.join(script_path, 'bm25_conf.yaml')
+with open(config_path, 'r') as f:
+    config = yaml.safe_load(f)
+
+# Define paths from config
+INDEX_DIR = str(os.path.join(project_path, config['bm25']['index_dir']))
+QUERIES_FILE = str(os.path.join(project_path, config['data']['data_dir'], config['data']['queries_file']))
+RUN_FILE = str(os.path.join(project_path, config['general']['output_dir'], 'run_bm25.txt'))
+
+# BM25 parameters from config
+
+# Controls term frequency scaling
+k1_range = config['bm25']['k1 range']
+# Controls document length normalization
+b_range = config['bm25']['b range']
+# Controls how many documents are returned for each query
+top_k = config['bm25'].get('top_k', 25)
+
+
+BM25 = BM25Baseline(index_path=INDEX_DIR,
+                    queries_file_path=QUERIES_FILE,
+                    run_file_path=RUN_FILE)
+
+# Trying every parameter permutation
+for k in k1_range:
+    for b in b_range:
+        # Search
+        BM25.run_search(k1=k, b=b, top_k=top_k)
+        break
+        # Evaluate
+        # TODO
+        # Save evaluation result
+        # TODO
+
+
